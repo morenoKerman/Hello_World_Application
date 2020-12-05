143afa55d883a3f3430d8fd1b5cf1387b7f22ab7
Moreno <65689078+morenoKerman@users.noreply.github.com>
2020-12-05 19:37:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworldapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.helloworldapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextTextPersonName;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this
+                );
+                builder.setTitle("Hello " + editTextTextPersonName.getText());
+                builder.setMessage("Welcome to my Hello World app");
+
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
